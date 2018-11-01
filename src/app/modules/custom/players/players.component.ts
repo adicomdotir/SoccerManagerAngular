c52845faa6c31ee8fe51be94bd3dad008e7cdad1
Adicom.ir <adicom.ir@gmail.com>
2018-11-02 01:16:17 +0330

    Add filter to players page

--- src/app/modules/custom/players/players.component.ts
+++ src/app/modules/custom/players/players.component.ts
@@ -16,4 +16,60 @@ export class PlayersComponent implements OnInit {
         this.players = this.storage.getPlayers();
     }
 
+    filterSelect(value, filter) {
+        if (filter == 4) {
+            switch (value) {
+                case '1': this.players = this.storage.getPlayers();
+                    break;
+                case '2': this.players = this.storage.getPlayers().filter(x => x.retired == false);
+                    break;
+                case '3': this.players = this.storage.getPlayers().filter(x => x.retired == true);
+                    break;
+            }
+        } else if (filter == 1) {
+            switch (value) {
+                case '1': this.players = this.storage.getPlayers().sort((a, b) => {
+                        var nameA = a.name.toUpperCase(); // ignore upper and lowercase
+                        var nameB = b.name.toUpperCase(); // ignore upper and lowercase
+                        if (nameA < nameB) {
+                            return -1;
+                        }
+                        if (nameA > nameB) {
+                            return 1;
+                        }
+                        // names must be equal
+                        return 0;
+                    });
+                    break;
+                case '2': this.players = this.storage.getPlayers().sort((a, b) => {
+                    var nameA = a.name.toUpperCase(); // ignore upper and lowercase
+                    var nameB = b.name.toUpperCase(); // ignore upper and lowercase
+                    if (nameA < nameB) {
+                        return 1;
+                    }
+                    if (nameA > nameB) {
+                        return -1;
+                    }
+                    // names must be equal
+                    return 0;
+                });
+                    break;
+            }
+        } else if (filter == 2) {
+            switch (value) {
+                case '1': this.players = this.storage.getPlayers().sort((a, b) => a.age - b.age);
+                    break;
+                case '2': this.players = this.storage.getPlayers().sort((a, b) => b.age - a.age);
+                    break;
+            }
+        } else if (filter == 3) {
+            switch (value) {
+                case '1': this.players = this.storage.getPlayers().sort((a, b) => a.overall - b.overall);
+                    break;
+                case '2': this.players = this.storage.getPlayers().sort((a, b) => b.overall - a.overall);
+                    break;
+            }
+        }
+    }
+
 }
